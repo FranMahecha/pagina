a5faddfed489732c10d29758e1a37f381858e887
FranciscoM <fmahecha@consorcioqutubminar.com>
2024-06-20 22:53:26 -0500

    segunda version

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Función que cambia el texto del título y el subtítulo
+function changeText() {
+    document.getElementById('title').innerText = 'Hola Mundo'; // Cambia el texto del título
+    document.getElementById('subtitle').innerText = '¡Has cambiado el texto!'; // Cambia el texto del subtítulo
+}
